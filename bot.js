b41f92392d475c90a85fc745beeba286857f9f6b
giacomino100 <giacomo.inghilleri@gmail.com>
2022-12-19 12:35:46 +0100

    first

---
+++ bot.js
@@ -0,0 +1,58 @@
+import TelegramBot from 'node-telegram-bot-api';
+import fetch from 'node-fetch';
+
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '5696084881:AAHognvG1xaAkfLjciD40mU3SQZVsDcDIdQ';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/start/, async (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Benvenuto " + msg.chat.first_name);
+
+
+});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/eventi/, async (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  try {
+    fetch('http://localhost:3000/events').then(async res => {
+        const json = await res.json();
+        bot.sendMessage(chatId, json.events.toString());
+    }).catch( err => {
+        bot.sendMessage(chatId, "err");
+
+    })
+
+  } catch (error) {
+    
+  }
+
+
+
+    //bot.sendMessage(chatId, res);
+
+
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
